90553837e14faccaada168e0ee528476726c9882
AmanWorku <amexworku2014@gmail.com>
2023-05-12 07:28:53 +0300

    Add content to public recipes controller

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,12 @@
+class PublicRecipesController < ApplicationController
+    def index
+      @recipes = public_recipes
+    end
+  
+    private
+  
+    def public_recipes
+      Recipe.where(public: true).order(created_at: :desc)
+    end
+  end
+  
