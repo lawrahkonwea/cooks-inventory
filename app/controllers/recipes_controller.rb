3e03829c22094fe86645ae6ccefd3eed71b9585b
AmanWorku <amexworku2014@gmail.com>
2023-05-12 07:55:20 +0300

    Fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,11 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_recipe, only: %i[ show edit update destroy ]
+  before_action :set_recipe, only: %i[show edit update destroy]
 
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.public_recipes.or(current_user.recipes.where(public_recipe: false))
   end
-  
 
   # GET /recipes/1 or /recipes/1.json
   def show
@@ -19,27 +18,26 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /recipes or /recipes.json
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
-    @recipe.public_recipe = params[:recipe][:public_recipe] == "1"
-  
+    @recipe.public_recipe = params[:recipe][:public_recipe] == '1'
+
     if @recipe.save
       redirect_to @recipe, notice: 'Recipe was successfully created.'
     else
       render :new
     end
   end
-  
+
   # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -63,6 +61,7 @@ class RecipesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   private
+
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
