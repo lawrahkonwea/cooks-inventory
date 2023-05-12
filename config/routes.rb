66602f15cb7597ca0c872e21ed60c571ffaa8247
AmanWorku <amexworku2014@gmail.com>
2023-05-12 11:43:08 +0300

    Add the button and functionality in the recipe details

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :inventories, only: [:index, :show, :create, :destroy]
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods, path: 'food', only: [:destroy, :new, :create]
+  end
   resources :foods
   resources :public_recipes
   # Defines the root path route ("/")
