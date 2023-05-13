5d85b8b2fa635f42207ea3e8a1a28db673f8e7b1
AmanWorku <amexworku2014@gmail.com>
2023-05-13 07:43:55 +0300

    Fix conflicts

--- db/migrate/20230510183811_add_columns_to_food.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToFood < ActiveRecord::Migration[7.0]
-  def change
-    add_column :foods, :name, :string
-    add_column :foods, :measurement_unit, :string
-    add_column :foods, :price, :decimal
-  end
-end
